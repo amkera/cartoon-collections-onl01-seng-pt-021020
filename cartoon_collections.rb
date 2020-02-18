9a028a6236206e2b492f4b9f7cc2f69f7054b82c
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-18 21:02:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
 end
+roll_call_dwarves(dwarves)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+    array.map {|name| "#{name}!".capitalize}
 end
+summon_captain_planet(planeteer_calls)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def long_planeteer_calls(array)
+    i = 0
+    if array.any? {|i| i.length > 4} #returns true when it reaches index 3
+        return true
+    else
+        return false
+    end
+    i += 1
+end
+long_planeteer_calls(call_screams)
+
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end
